9117f3e96fc5f0da095c98300dcc8eef21fd31d9
Harduex <simeon_simka@mail.bg>
2020-07-07 16:48:03 +0300

    Changed listing articles to fetch/ajax and added about page

--- ScienceNewsBlog/Controllers/HomeController.cs
+++ ScienceNewsBlog/Controllers/HomeController.cs
@@ -20,6 +20,10 @@ namespace ScienceNewsBlog.Controllers
 
             return View(articles);
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- ScienceNewsBlog/Data/Models/Article.cs
+++ ScienceNewsBlog/Data/Models/Article.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -24,6 +26,8 @@ namespace ScienceNewsBlog.Data.Models
 
         [DisplayName("Photo Url: ")]
         public string PhotoUrl { get; set; }
+
         public DateTime CreatedDate { get; set; }
+
     }
 }
